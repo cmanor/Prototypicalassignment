1cced9f2eef0f4f5b921daa102860581df956beb
cmanor <101116379+cmanor@users.noreply.github.com>
2022-03-28 15:45:04 -0400

    first2 parts done

---
+++ index.js
@@ -0,0 +1,15 @@
+class Person
+{
+    constructor()
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.favoriteColor = favoriteColor;
+        this.favoriteNumber = favoriteNumber;
+        this.favoriteFoods = [];
+    }
+    fullName()
+    {
+        return this.firstname + this.lastName;
+    }
+}
