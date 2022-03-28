8f52f8b873e44e0ec265acfee256c39360c669a8
cmanor <101116379+cmanor@users.noreply.github.com>
2022-03-28 16:57:11 -0400

    end of day

--- index.js
+++ index.js
@@ -7,9 +7,24 @@ class Person
         this.favoriteColor = favoriteColor;
         this.favoriteNumber = favoriteNumber;
         this.favoriteFoods = [];
-    }
-    fullName()
-    {
-        return this.firstname + this.lastName;
+        this.family = [];
     }
 }
+
+Person.prototype.fullName = function()
+{
+    return this.firstname + this.lastName;
+}
+
+Person.prototype.toString = function()
+{
+    return this.firstName + this.lastName + "Favorite Color: " + this.favoriteColor + this.favoriteNumber;
+}
+
+Person.prototype.addToFamily = function(familyMember)
+{
+    return this.family.push(familyMember)
+}
+
+let deez = new Person("deez", "johnson", "green", "69", ["food1", "food2", "food3"]);
+let knutz = new Person("knutz", "peterson", "brown", "69", ["food4", "food5", "food6"]);
